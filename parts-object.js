3cd3a821d72425ca6f185c8e81d3e240ad1015c4
Kyle Booth <kbooth1000@gmail.com>
2019-08-02 12:32:40 -0400

    Add replacement parts list to final modal

---
+++ parts-object.js
@@ -0,0 +1,172 @@
+var imgUrlACondenserEvapCoil =
+    'https://img1.wsimg.com/isteam/ip/ec3d7ae1-84c5-494d-939d-ab7eac153ebf/Air%20conditioner%20and%20heat%20pump%20with%20coil.jpg/:/',
+  imgUrlFurnaceEvapCoil =
+    'https://img1.wsimg.com/isteam/ip/ec3d7ae1-84c5-494d-939d-ab7eac153ebf/furnaceevaporatorcoilreplacement.png/:/',
+  imgUrlACCondenserUnitOnly =
+    'https://img1.wsimg.com/isteam/ip/ec3d7ae1-84c5-494d-939d-ab7eac153ebf/airConditioner%201%20resized.png/:/,
+  imgUrlHeatPumpUnitOnly =
+    'https://img1.wsimg.com/isteam/ip/ec3d7ae1-84c5-494d-939d-ab7eac153ebf/heatPump%202%20resized.png/:/',
+  imgUrlEvapCoil =
+    'https://img1.wsimg.com/isteam/ip/ec3d7ae1-84c5-494d-939d-ab7eac153ebf/uncased%20coil%201%20resized.png/:/',
+  imgUrlAirHandler =
+    'https://img1.wsimg.com/isteam/ip/ec3d7ae1-84c5-494d-939d-ab7eac153ebf/airHandler%201%20resized.png/:/',
+  imgUrlFurnace =
+    'https://img1.wsimg.com/isteam/ip/ec3d7ae1-84c5-494d-939d-ab7eac153ebf/gasFurnace%201%20resized%203.png/:/';
+
+var replacementParts = [
+  {
+    category: 'A/C Condenser &amp; Evaporator Coil Replacement',
+    sf800: {
+      name: '1.5 Ton',
+      ton: 1.5,
+      seer: 1111,
+      btu: 1111,
+      imgUrl: imgUrlACondenserEvapCoil,
+      price: 2999,
+      desc: 'Description.',
+      details: {
+        model: 'XXXX',
+        longDesc:
+          '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p> \n   <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>'
+      }
+    },
+    sf1000: {
+      name: '2.0 Ton',
+      ton: 2.0,
+      price: 3525,
+      imgUrl: imgUrlACondenserEvapCoil,
+      desc: 'Description.',
+      seer: 1111,
+      btu: 1111,
+      details: {}
+    },
+    sf1200: {
+      name: '2.5 Ton',
+      ton: 2.5,
+      price: 3715,
+      imgUrl: imgUrlACondenserEvapCoil,
+      desc: 'Description.',
+      seer: 1111,
+      btu: 1111,
+      details: {}
+    },
+    sf1400: {
+      name: '3.0 Ton',
+      ton: 3.0,
+      price: 3825,
+      imgUrl: imgUrlACondenserEvapCoil,
+      desc: 'Description.',
+      seer: 1111,
+      btu: 1111,
+      details: {}
+    },
+    sf1700: {
+      name: '3.5 Ton',
+      ton: 3.5,
+      price: 3999,
+      imgUrl: imgUrlACondenserEvapCoil,
+      desc: 'Description.',
+      seer: 1111,
+      btu: 1111,
+      details: {}
+    },
+    sf2000: {
+      name: '4.0 Ton',
+      ton: 4.0,
+      price: 4050,
+      imgUrl: imgUrlACondenserEvapCoil,
+      desc: 'Description.',
+      seer: 1111,
+      btu: 1111,
+      details: {}
+    },
+    sf2500: {
+      name: '5.0 Ton',
+      ton: 5.0,
+      price: 4279,
+      imgUrl: imgUrlACondenserEvapCoil,
+      desc: 'Description.',
+      seer: 1111,
+      btu: 1111,
+      details: {}
+    }
+  },
+
+  {
+    category: 'Furnace &amp; Evaporator Coil Replacement',
+    sf800: {
+      name: '1.5 Ton',
+      ton: 1.5,
+      seer: 1111,
+      btu: 1111,
+      imgUrl: imgUrlFurnaceEvapCoil,
+      price: 2825,
+      desc: 'Description.',
+      details: {
+        model: 'XXXX',
+        longDesc:
+          '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p> \n   <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>'
+      }
+    },
+    sf1000: {
+      name: '2.0 Ton',
+      ton: 2.0,
+      price: 2900,
+      imgUrl: imgUrlFurnaceEvapCoil,
+      desc: 'Description.',
+      seer: 1111,
+      btu: 1111,
+      details: {}
+    },
+    sf1200: {
+      name: '2.5 Ton',
+      ton: 2.5,
+      price: 2999,
+      imgUrl: imgUrlFurnaceEvapCoil,
+      desc: 'Description.',
+      seer: 1111,
+      btu: 1111,
+      details: {}
+    },
+    sf1400: {
+      name: '3.0 Ton',
+      ton: 3.0,
+      price: 3175,
+      imgUrl: imgUrlFurnaceEvapCoil,
+      desc: 'Description.',
+      seer: 1111,
+      btu: 1111,
+      details: {}
+    },
+    sf1700: {
+      name: '3.5 Ton',
+      ton: 3.5,
+      price: 3299,
+      imgUrl: imgUrlFurnaceEvapCoil,
+      desc: 'Description.',
+      seer: 1111,
+      btu: 1111,
+      details: {}
+    },
+    sf2000: {
+      name: '4.0 Ton',
+      ton: 4.0,
+      price: 3375,
+      imgUrl: imgUrlFurnaceEvapCoil,
+      desc: 'Description.',
+      seer: 1111,
+      btu: 1111,
+      details: {}
+    },
+    sf2500: {
+      name: '5.0 Ton',
+      ton: 5.0,
+      price: 3425,
+      imgUrl: imgUrlFurnaceEvapCoil,
+      desc: 'Description.',
+      seer: 1111,
+      btu: 1111,
+      details: {}
+    }
+  }
+];
